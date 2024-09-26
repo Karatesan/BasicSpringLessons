1daff651cb60f31da4da2b8a77790fc87b5b92c3
Maciej Gomulec <maciej.gomulec@fdmgroup.com>
2024-09-26 09:19:27 +0200

    dasdas

--- src/main/java/org/example/contoller/TestController.java
+++ src/main/java/org/example/contoller/TestController.java
@@ -1,13 +1,13 @@
 package org.example.contoller;
 
+import org.example.dto.SaveUserRequestDto;
 import org.example.model.MyUser;
 import org.example.model.TestDto;
 import org.example.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 //@RequestMapping("/test")
@@ -17,30 +17,69 @@ public class TestController {
     UserService userService;
 
     @GetMapping
-    public String test(){
-        return "dsadsadsa";
+    public String test() {
+        return "zmiana";
     }
 
     @GetMapping("/dto")
-    public TestDto test1(){
-        return new TestDto("test","dasdasdas");
+    public TestDto test1() {
+        return new TestDto("test", "dasdasdas");
     }
 
-    @GetMapping("/save")
-    public void test2(@RequestParam(name = "username") String username, @RequestParam(name = "username") String email){
+    //http://localhost:8080/Spring/save?name=Iza&email=dasdasda
+    @PostMapping("/save")
+    public void createUser(@RequestParam("name") String name,
+                           @RequestParam(value = "email",
+                                   required = false,
+                                   defaultValue = "NieMaMaila@jdadjo")
+                           String email)
+    {
         MyUser user = new MyUser();
         user.setEmail(email);
-        user.setName(username);
+        user.setName(name);
+        userService.saveUser(user);
+    }
 
+//http://localhost:8080/Spring-1.0-SNAPSHOT/saveBigData?name=Maciek&email=testemail
+    //wrzuca wszyskie parametry z linku (te po ? ) do obiektu java, nie jest potrzebna anotacja zadna
+    @PostMapping("/saveBigData")
+    public void createUserBig(SaveUserRequestDto requestDto) {
+        MyUser user = new MyUser();
+        user.setEmail(requestDto.email());
+        user.setName(requestDto.name());
+        userService.saveUser(user);
+    }
+
+    //RequestBody
+    //http://localhost:8080/Spring/get/Iza
+    @GetMapping("/get/{username}")
+    public MyUser getUserByName(@PathVariable("username") String username) {
+        return userService.findUserByName(username);
+    }
+
+    // body przechwytuje informacje z body requesta - zeby tego uzyc musisz uzyc zewnetrznej aplickajci
+    @PostMapping("/saveBody")
+    public void createSecureUser(@RequestBody SaveUserRequestDto requestDto) {
+        MyUser user = new MyUser();
+        user.setEmail(requestDto.email());
+        user.setName(requestDto.name());
+        userService.saveUser(user);
+    }
+
+    @PostMapping("/saveQuery")
+    public void saveUserQuery(MyUser user){
         userService.saveUser(user);
     }
 
     @GetMapping("/get")
-    public MyUser test3(){
-
-
+    public MyUser test3() {
         return userService.findAllUsers().get(0);
     }
 
+    @GetMapping("/getAll")
+    public List<MyUser> getAll() {
+
+        return userService.findAllUsers();
+    }
 
 }
---
+++ src/main/java/org/example/dto/SaveUserRequestDto.java
@@ -0,0 +1,4 @@
+package org.example.dto;
+
+public record SaveUserRequestDto( String name, String email ) {
+}
